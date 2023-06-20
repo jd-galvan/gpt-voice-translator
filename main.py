45ed5afdd64417ed779ff7a1084d63354e2de5f5
jd-galvan <jdgsuazo@gmail.com>
2023-06-20 00:00:02 -0500

    Refactor folders

--- main.py
+++ main.py
@@ -24,17 +24,17 @@ while True:
 
     myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
     sd.wait()  # Wait until recording is finished
-    write('input.mp3', fs, myrecording)  # Save file
+    write('./audios/input.mp3', fs, myrecording)  # Save file
 
     print("Traduciendo...")
-    playsound("waiting.mp3")
+    playsound("./audios/waiting.mp3")
 
     # Translate
     openai.api_key = os.environ.get("GPT_ACCESS_KEY")
-    audio_file = open("input.mp3", "rb")
+    audio_file = open("./audios/input.mp3", "rb")
     response = openai.Audio.translate("whisper-1", audio_file)
     print(f"Traducción: {response['text']}")
 
     t1 = gTTS(response["text"], lang="en")
-    t1.save("output.mp3")
-    playsound("output.mp3")
+    t1.save("./audios/output.mp3")
+    playsound("./audios/output.mp3")
